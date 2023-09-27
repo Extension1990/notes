61b737b3879311b2cac317a3d0e575882fb82dee
Extension1990 <extensiontlhareseng@gmail.com>
2023-09-27 20:41:59 +0200

    Frontend setup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+const dotenv = require('dotenv');
 const notesRoutes = require('./routes/notes.routes');
+
+dotenv.config();
 
 app.use('/api/notes', notesRoutes);
 
-app.listen(5000, console.log("Server running on PORT 5000..."));
+const PORT = process.env.PORT || 5000;  
+
+app.listen(PORT, console.log(`Server running on PORT ${PORT}...`));
