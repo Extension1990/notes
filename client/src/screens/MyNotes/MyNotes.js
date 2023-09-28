1b8dd197a21abcecebac979503935924f0537869
Extension1990 <extensiontlhareseng@gmail.com>
2023-09-28 20:14:22 +0200

    Our client and Server apps are connected using axios

--- client/src/screens/MyNotes/MyNotes.js
+++ client/src/screens/MyNotes/MyNotes.js
@@ -1,35 +1,91 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MainScreen from '../../components/MainScreen/MainScreen';
 import { Link } from 'react-router-dom';
-import { Button, Card } from 'react-bootstrap';
+import { Badge, Button, Card, Accordion } from 'react-bootstrap';
+import axios from 'axios';
 
 const MyNotes = () => {
+  const [notes, setNotes] = useState([]);
+
+  const deleteHandler = (id) => {
+    if(window.confirm("Are you sure")) {
+
+    }
+  };
+
+  const fetchNotes = async () => {
+    const { data } = await axios.get('/api/notes');
+    setNotes(data);
+  }
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
   return (
     <MainScreen title="Welcome back John Bower...">
     <Link to="/create-note" style={{ textDecoration: 'none' }}>
         <Button size="lg" className="createButton">Create New Note</Button>
     </Link>
-    <Card className='mt-3'>
-      <Card.Header className='d-flex'>
-        Quote
-        <div>
-          <Button variant='primary'>Edit</Button>
-          <Button variant='danger'>Delete</Button>
-        </div>
-      </Card.Header>
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p>
-            {' '}
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-            posuere erat a ante.{' '}
-          </p>
-          <footer className="blockquote-footer">
-            Someone famous in <cite title="Source Title">Source Title</cite>
-          </footer>
-        </blockquote>
-      </Card.Body>
-    </Card>
+    {notes.map(note => {
+      return (
+        <Accordion className='mt-3' key={note._id}>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>
+                  <span
+                    style={{
+                      color: "white",
+                      textDecoration: "none",
+                      flex: 1,
+                      cursor: "pointer",
+                      alignSelf: "center",
+                      fontSize: 18
+                    }}
+                  >
+                    { note.title }
+                  </span>
+            </Accordion.Header>
+            <Accordion.Body>
+              <Card style={{ marginTop: 15, marginBottom: 15 }}>
+                <Card.Header style={{ display: "flex" }} className='bg-white'>
+                  <span
+                    style={{
+                      color: "black",
+                      textDecoration: "none",
+                      flex: 1,
+                      cursor: "pointer",
+                      alignSelf: "center",
+                      fontSize: 18
+                    }}
+                  >
+                    { note.title }
+                  </span>
+                  <div>
+                    <Button variant='primary' href={`/note/${note._id}`}>Edit</Button>
+                    <Button variant='danger' className='mx-2' onClick={() => deleteHandler(note._id)}>Delete</Button>
+                  </div>
+                </Card.Header>
+                <Card.Body>
+                  <h4>
+                    <Badge variant="success">
+                      Category - { note.category }
+                    </Badge>
+                  </h4>
+                  <blockquote className="blockquote mb-0">
+                    <p>
+                      { note.content }
+                    </p>
+                    <footer className="blockquote-footer">
+                      Created On: <cite title="Source Title">22/09/2023</cite>
+                    </footer>
+                  </blockquote>
+                </Card.Body>
+              </Card>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      )
+    })}
     </MainScreen>
   )
 }
