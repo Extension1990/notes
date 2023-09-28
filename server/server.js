607ab264300913d8fa707edb42c56dad6c2fb7b5
Extension1990 <extensiontlhareseng@gmail.com>
2023-09-28 21:23:13 +0200

    We have connected our app to mongodb

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const app = express();
 const dotenv = require('dotenv');
 const notesRoutes = require('./routes/notes.routes');
+const connectDB = require('./config/db');
 
+const app = express();
 dotenv.config();
+connectDB();
 
 app.use('/api/notes', notesRoutes);
 
-const PORT = process.env.PORT || 5000;  
+const PORT = process.env.PORT || 9000; 
 
 app.listen(PORT, console.log(`Server running on PORT ${PORT}...`));
